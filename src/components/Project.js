81a7e0ad41954d11de47c5c9c0a7c14a26e03124
Mike Feldstein <msfeldstein@gmail.com>
2019-05-06 20:02:11 -0700

    Make projects work on dsktop

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,8 +3,10 @@ import './Project.css'
 export default function Project(props) {
     const img = props.thumb ? <img className="Project_thumb" src={props.thumb} /> : null
     return <div key={props.title} className="Project">
-        <div className="Project_title">{props.title}</div>
-        <div className="Project_description">{props.description}</div>
+        <div className="Project_info">
+            <div className="Project_title">{props.title}</div>
+            <div className="Project_description">{props.description}</div>
+        </div>
         {img}
     </div>
 }
